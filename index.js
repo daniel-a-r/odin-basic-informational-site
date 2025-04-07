5b1c79705b6fed6b00347006bc83c6c828ac1b61
daniel-a-r <daniel.a-r@outlook.com>
2025-04-07 16:15:00 -0400

    refactor using express

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
-import http from 'node:http';
 import path from 'node:path';
-import fs from 'node:fs/promises';
+import express from 'express';
 
 const PORT = process.env.PORT || 5000;
 
-const server = http.createServer((req, res) => {
-  const sendResponse = async (fileName, statusCode = 200) => {
-    const filePath = path.join(import.meta.dirname, 'public', fileName);
-    const contentType = 'text/html';
-    try {
-      const content = await fs.readFile(filePath);
-      res.writeHead(statusCode, { 'Content-Type': contentType });
-      res.end(content, 'utf8');
-    } catch (err) {
-      console.error(err);
-      res.writeHead(500);
-      res.end(`Server error: ${err.code}`);
-    }
-  };
+const app = express();
 
-  switch (req.url) {
-    case '/':
-      sendResponse('index.html');
-      break;
-    case '/about':
-      sendResponse(`${req.url}.html`);
-      break;
-    case '/contact-me':
-      sendResponse(`${req.url}.html`);
-    default:
-      sendResponse('404.html', 404);
-      break;
-  }
+const createFilePath = (fileName) => {
+  const filePath = path.join(import.meta.dirname, 'public', fileName);
+  return filePath;
+}
+
+app.get('/', (req, res) => {
+  const filePath = createFilePath('index.html')
+  res.sendFile(filePath);
 });
 
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+app.get('/about', (req, res) => {
+  const filePath = createFilePath(`${req.url}.html`);
+  res.sendFile(filePath);
+});
+
+app.get('/contact-me', (req, res) => {
+  const filePath = createFilePath(`${req.url}.html`);
+  res.sendFile(filePath);
+});
+
+app.use((req, res) => {
+  const filePath = createFilePath('404.html');
+  res.status(404).sendFile(filePath);
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
